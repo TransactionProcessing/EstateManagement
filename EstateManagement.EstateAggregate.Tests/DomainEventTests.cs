ab068e97e4be45c44416c2e5e4f2975ba772bbe8
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2019-10-25 13:16:37 +0100

    Work completed

---
+++ EstateManagement.EstateAggregate.Tests/DomainEventTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstateManagement.EstateAggregate.Tests
+{
+    using Estate.DomainEvents;
+    using Shouldly;
+    using Xunit;
+
+    public class DomainEventTests
+    {
+        [Fact]
+        public void EstateCreatedEvent_CanBeCreated_IsCreated()
+        {
+            EstateCreatedEvent estateCreatedEvent = EstateCreatedEvent.Create(TestData.EstateId, TestData.EstateName);
+
+            estateCreatedEvent.ShouldNotBeNull();
+            estateCreatedEvent.AggregateId.ShouldBe(TestData.EstateId);
+            estateCreatedEvent.EventCreatedDateTime.ShouldNotBe(DateTime.MinValue);
+            estateCreatedEvent.EventId.ShouldNotBe(Guid.Empty);
+            estateCreatedEvent.EstateId.ShouldBe(TestData.EstateId);
+            estateCreatedEvent.EstateName.ShouldBe(TestData.EstateName);
+        }
+    }
+}
--- EstateManagement.EstateAggregate.Tests/EstateAggregateTests.cs
+++ EstateManagement.EstateAggregate.Tests/EstateAggregateTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace EstateManagement.EstateAggregate.Tests
 {
+    using System;
     using Shouldly;
     using Xunit;
 
@@ -11,6 +12,45 @@
             EstateAggregate aggregate = EstateAggregate.Create(TestData.EstateId);
 
             aggregate.AggregateId.ShouldBe(TestData.EstateId);
+        }
+
+        [Fact]
+        public void EstateAggregate_Register_IsCreated()
+        {
+            EstateAggregate aggregate = EstateAggregate.Create(TestData.EstateId);
+            aggregate.Register(TestData.EstateName);
+
+            aggregate.AggregateId.ShouldBe(TestData.EstateId);
+            aggregate.EstateName.ShouldBe(TestData.EstateName);
+            aggregate.IsCreated.ShouldBeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void EstateAggregate_Register_InvalidEstateName_ErrorThrown(String estateName)
+        {
+            EstateAggregate aggregate = EstateAggregate.Create(TestData.EstateId);
+            ArgumentNullException exception = Should.Throw<ArgumentNullException>(() =>
+                                                {
+                                                    aggregate.Register(estateName);
+                                                });
+            
+            exception.Message.ShouldContain("Estate name must be provided when registering a new estate");
+        }
+
+        [Fact]
+        public void EstateAggregate_Register_EstateAlreadyCreated_ErrorThrown()
+        {
+            EstateAggregate aggregate = EstateAggregate.Create(TestData.EstateId);
+            aggregate.Register(TestData.EstateName);
+
+            InvalidOperationException exception = Should.Throw<InvalidOperationException>(() =>
+                                                                                  {
+                                                                                      aggregate.Register(TestData.EstateName);
+                                                                                  });
+
+            exception.Message.ShouldContain($"Estate with name {TestData.EstateName} has already been created");
         }
     }
 }
