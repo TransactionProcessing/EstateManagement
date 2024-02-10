c8964ee0073bb717a25f397b7ffebbd7ada98854
Stuart Ferguson <stuart_ferguson1development@outlook.com>
2024-02-10 08:18:05 +0000

    .

--- EstateManagement.BusinessLogic/EventHandling/FloatEventHandler.cs
+++ EstateManagement.BusinessLogic/EventHandling/FloatEventHandler.cs
@@ -1,48 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EstateManagement.BusinessLogic.EventHandling
-{
+﻿namespace EstateManagement.BusinessLogic.EventHandling{
     using System.Threading;
-    using EstateManagement.Repository;
+    using System.Threading.Tasks;
+    using Repository;
     using Shared.DomainDrivenDesign.EventSourcing;
     using Shared.EventStore.EventHandling;
     using TransactionProcessor.Float.DomainEvents;
-    using TransactionProcessor.Transaction.DomainEvents;
 
-    public class FloatDomainEventHandler : IDomainEventHandler
-    {
+    public class FloatDomainEventHandler : IDomainEventHandler{
+        #region Fields
+
         private readonly IEstateReportingRepository EstateReportingRepository;
+
+        #endregion
+
+        #region Constructors
 
         public FloatDomainEventHandler(IEstateReportingRepository estateReportingRepository){
             this.EstateReportingRepository = estateReportingRepository;
         }
 
+        #endregion
+
+        #region Methods
+
         public async Task Handle(IDomainEvent domainEvent,
-                                 CancellationToken cancellationToken)
-        {
+                                 CancellationToken cancellationToken){
             await this.HandleSpecificDomainEvent((dynamic)domainEvent, cancellationToken);
         }
 
         private async Task HandleSpecificDomainEvent(FloatCreatedForContractProductEvent domainEvent,
-                                                     CancellationToken cancellationToken)
-        {
+                                                     CancellationToken cancellationToken){
             await this.EstateReportingRepository.CreateFloat(domainEvent, cancellationToken);
         }
 
         private async Task HandleSpecificDomainEvent(FloatCreditPurchasedEvent domainEvent,
-                                                     CancellationToken cancellationToken)
-        {
+                                                     CancellationToken cancellationToken){
             await this.EstateReportingRepository.CreateFloatActivity(domainEvent, cancellationToken);
         }
 
         private async Task HandleSpecificDomainEvent(FloatDecreasedByTransactionEvent domainEvent,
-                                                     CancellationToken cancellationToken)
-        {
+                                                     CancellationToken cancellationToken){
             await this.EstateReportingRepository.CreateFloatActivity(domainEvent, cancellationToken);
         }
+
+        #endregion
     }
 }
