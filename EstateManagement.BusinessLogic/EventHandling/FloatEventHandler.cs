46dacc1b63da9cd6a6584a543e9588716024dec3
Stuart Ferguson <stuart_ferguson1development@outlook.com>
2024-02-10 08:14:04 +0000

    load float events to the Read Model

---
+++ EstateManagement.BusinessLogic/EventHandling/FloatEventHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstateManagement.BusinessLogic.EventHandling
+{
+    using System.Threading;
+    using EstateManagement.Repository;
+    using Shared.DomainDrivenDesign.EventSourcing;
+    using Shared.EventStore.EventHandling;
+    using TransactionProcessor.Float.DomainEvents;
+    using TransactionProcessor.Transaction.DomainEvents;
+
+    public class FloatEventHandler : IDomainEventHandler
+    {
+        private readonly IEstateReportingRepository EstateReportingRepository;
+
+        public FloatEventHandler(IEstateReportingRepository estateReportingRepository){
+            this.EstateReportingRepository = estateReportingRepository;
+        }
+
+        public async Task Handle(IDomainEvent domainEvent,
+                                 CancellationToken cancellationToken)
+        {
+            await this.HandleSpecificDomainEvent((dynamic)domainEvent, cancellationToken);
+        }
+
+        private async Task HandleSpecificDomainEvent(FloatCreatedForContractProductEvent domainEvent,
+                                                     CancellationToken cancellationToken)
+        {
+            await this.EstateReportingRepository.CreateFloat(domainEvent, cancellationToken);
+        }
+
+        private async Task HandleSpecificDomainEvent(FloatCreditPurchasedEvent domainEvent,
+                                                     CancellationToken cancellationToken)
+        {
+            await this.EstateReportingRepository.CreateFloatActivity(domainEvent, cancellationToken);
+        }
+
+        private async Task HandleSpecificDomainEvent(FloatDecreasedByTransactionEvent domainEvent,
+                                                     CancellationToken cancellationToken)
+        {
+            await this.EstateReportingRepository.CreateFloatActivity(domainEvent, cancellationToken);
+        }
+    }
+}
---
+++ EstateManagement.Database/Entities/Float.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstateManagement.Database.Entities
+{
+    public class Float
+    {
+        public Guid FloatId { get; set; }
+
+        public Guid EstateId { get; set; }
+
+        public Guid ContractId { get; set; }
+
+        public Guid ProductId { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
